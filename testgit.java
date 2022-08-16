7c383b118bb04dda9e08dd0f337780ffcffddd9a
anjhana srivijay <anjhanasrivijay@gmail.com>
2022-08-16 13:53:40 +0200

    original file

--- testgit.java
+++ testgit.java
@@ -7,10 +7,21 @@ public class testgit {
         System.out.println("Greetings");
         System.out.println("Select one or two(1/2):  ");
         int input = in.nextInt();
+
         if (input == 1) {
             System.out.println("Hi, How are you doing today :)");
+            System.out.println(testgit.hello());
+
         } else {
             System.out.println("hello and bye");
+            System.out.println(testgit.hello());
+
         }
+
+    }
+
+    public static String hello(){
+        return "Thank you for using Greeting!  See you soon!";
+
     }
 }
