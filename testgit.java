7b0dfd0558bf5f606d36215f2cdd159001bcde04
anjhana srivijay <anjhanasrivijay@gmail.com>
2022-08-16 13:39:25 +0200

    original file

--- testgit.java
+++ testgit.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class testgit {
+    static Scanner in = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("hello and bye");
+        System.out.println("Greetings");
+        System.out.println("Select one or two(1/2):  ");
+        int input = in.nextInt();
+        if (input == 1) {
+            System.out.println("Hi, How are you doing today :)");
+        } else {
+            System.out.println("hello and bye");
+        }
     }
 }
